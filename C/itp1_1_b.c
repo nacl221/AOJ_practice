1af37adfc92d5abe5b156972549a99012c28c5e9
nacl221 <nacl221.dev@gmail.com>
2026-03-07 00:09:42 +0900

    Update itp1_1_b.c

--- C/itp1_1_b.c
+++ C/itp1_1_b.c
@@ -1,7 +1,7 @@
 int main(){
-    int x,y;
-    scanf("%d",&x);
-    y = x*x*x;
+    int x,y; //Define variable(integer) 変数(整数)の定義
+    scanf("%d",&x); //Standard input 標準入力
+    y = x*x*x;  //Substitution calculation and Arithmetic calculation 代入演算と算術演算
     printf("%d\n",y);
     return 0;
 }
