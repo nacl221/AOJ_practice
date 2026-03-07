510cfd53fd39b315a14bb1fb9c241fc1354a67f4
nacl221 <nacl221.dev@gmail.com>
2026-03-07 22:33:15 +0900

    Update itp1_1_c.c

--- C/itp1_1_c.c
+++ C/itp1_1_c.c
@@ -1,8 +1,8 @@
 int main(){
     int a,b,v,c;
-    scanf("%d %d",&a,&b);
+    scanf("%d %d",&a,&b); //scanf multiple variables in the line 複数の変数を一行でscanf
     v = a*b;
     c = 2*a+2*b;
-    printf("%d %d\n",v,c);
+    printf("%d %d\n",v,c); //printf multiple variables in the line 複数の変数を一行でprintf
     return 0;
 }
