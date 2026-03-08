0912157486aeb7f6c95d9eb9a5030a082c2e2e6f
nacl221 <nacl221.dev@gmail.com>
2026-03-09 00:03:08 +0900

    Update itp1_1_d.c

--- C/itp1_1_d.c
+++ C/itp1_1_d.c
@@ -1,9 +1,9 @@
 int main(){
     int S, h = 0, m = 0, s = 0;
     scanf("%d",&S);
-    h = S / 3600;
+    h = S / 3600; //calculate hour causion,its 3600s not 60m 時間の計算　注意、60分ではなく3600秒
     m = (S % 3600) / 60;
     s = S % 60;
-    printf("%d:%d:%d\n", h, m, s);
+    printf("%d:%d:%d\n", h, m, s); //Output to h:m:s h:m:sの順に出力
     return 0;
 }
