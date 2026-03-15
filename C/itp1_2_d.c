be4ac9b8ebc1b5d6373bc2e2e91c41ad393bbb69
nacl221 <nacl221.dev@gmail.com>
2026-03-15 22:54:50 +0900

    addC itp1_2_d.c

--- C/itp1_2_d.c
+++ C/itp1_2_d.c
@@ -1,7 +1,7 @@
 int main(){
     int W, H, x, y, r;
     scanf("%d %d %d %d %d", &W, &H, &x, &y, &r);
-    if ( W >= x + r && H >= y + r && x - r >= 0 && y - r >= 0 ){
+    if ( W >= x + r && H >= y + r && x - r >= 0 && y - r >= 0 ){ // don't cross the right side, top, left side and under 右側を超えないか、上側を超えないか、左側を超えないか、下側を超えないか確認
         printf("Yes\n");
     }
     else{
