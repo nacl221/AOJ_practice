a0648a9f5a87bf9b051e146ce3b1df76e5623b20
nacl221 <nacl221.dev@gmail.com>
2026-03-18 23:42:30 +0900

    addC itp1_3_b.c

--- C/itp1_3_b.c
+++ C/itp1_3_b.c
@@ -1,8 +1,8 @@
 int main(){
     int i, x;
-    for( i = 1; i <= 10000; i++ ){
+    for( i = 1; i <= 10000; i++ ){ // "less than 10,000" includes 10,000 10,000を超えないは10,000も含む
         scanf("%d", &x);
-        if ( x == 0 ) break;
+        if ( x == 0 ) break; // if meet the condition, break the loop 条件を満たしたのならば、ループから抜ける
         printf("Case %d: %d\n", i, x);
     }
     return 0;
