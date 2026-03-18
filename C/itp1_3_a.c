c488b12834151972cac580527f703b64e7f3edbd
nacl221 <nacl221.dev@gmail.com>
2026-03-18 23:22:51 +0900

    addC itp1_3_a.c

--- C/itp1_3_a.c
+++ C/itp1_3_a.c
@@ -1,6 +1,6 @@
 int main(){
     int i;
-    for ( i = 0; i < 1000; i++ ){
+    for ( i = 0; i < 1000; i++ ){ // must to write two ";" //( first definition; finish condition; repeat condition);
         printf("Hello World\n");
     }
     return 0;
