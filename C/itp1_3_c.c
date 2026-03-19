c6a0dd744c9ddd30b54daf1131fc3622117ac9ce
nacl221 <nacl221.dev@gmail.com>
2026-03-19 23:09:50 +0900

    add itp1_3_c.c

---
+++ C/itp1_3_c.c
@@ -0,0 +1,14 @@
+int main(){
+    int x, y, i, t;
+    for ( i = 0; i <= 3000; i++){
+        scanf("%d %d", &x, &y);
+        if ( x == 0 && y == 0) break;
+        if ( x > y ){
+             t = x;
+             x = y;
+             y = t;
+             printf("%d %d\n", x, y);
+        }
+        else printf("%d %d\n", x, y);
+    }
+}
