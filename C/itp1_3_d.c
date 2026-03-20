1fdfb62f2243baf1e13ab59dd09f216cb6a4e217
nacl221 <nacl221.dev@gmail.com>
2026-03-20 23:54:39 +0900

    addC itp1_3_d.c

--- C/itp1_3_d.c
+++ C/itp1_3_d.c
@@ -3,7 +3,7 @@ int main(){
     t = 0;
     scanf("%d %d %d", &a, &b, &c);
     for ( i = a; i <= b; i++){
-         if( c % i == 0){
+         if( c % i == 0){ // calculate the remainder of c to check if i is a divisor of c 余りを計算してiがcの約数かを判断する
             t = t + 1;
          }
          else;
