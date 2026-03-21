6217715fd9c1f4b168813af4e24e86d8704a0052
nacl221 <nacl221.dev@gmail.com>
2026-03-21 23:28:22 +0900

    addC itp1_4_a.c

--- C/itp1_4_a.c
+++ C/itp1_4_a.c
@@ -1,10 +1,10 @@
 int main(){
     int a, b, d, r;
-    double f;
+    double f; //define variable(rational numbers) 変数(有理数)の定義
     scanf("%d %d", &a, &b);
     d = a / b;
-    r = a % b;
-    f = 1.0000 * a / b;
-    printf("%d %d %.5f\n", d, r, f);
+    r = a % b; //calculation of remainder 余りの計算 
+    f = 1.0000 * a / b; //division of rational numbers 有理数の割り算
+    printf("%d %d %.5f\n", d, r, f); // display rational numbers .5 means 5 decimal places 有理数の表示 .5は小数第5位の意味
     return 0;
 }
