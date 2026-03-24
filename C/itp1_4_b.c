297e0622ae77f68ecac68d69c541486bbc37712e
nacl221 <nacl221.dev@gmail.com>
2026-03-24 21:04:18 +0900

    addC itp1_4_b.c

--- C/itp1_4_b.c
+++ C/itp1_4_b.c
@@ -1,7 +1,7 @@
 int main(){
     double r,S,L;
-    scanf("%lf", &r);
-    S = 1.00000 * r * r * 3.141592653589;
+    scanf("%lf", &r); //in this case, use %lf 
+    S = 1.00000 * r * r * 3.141592653589; // in this case, use 3.141592653589 instead of pie 今回はπの代わりに3.141592653589を用いる
     L = 1.00000 * 2 * r * 3.141592653589;
     printf("%.5f %.5f\n", S, L);
     return 0;
